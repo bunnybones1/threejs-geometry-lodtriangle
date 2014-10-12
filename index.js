cde21a5660b43f04b433dc774a1e230d3f39c05d
Tomasz Dysinski <oz@bunnybones.com>
2014-10-12 02:42:36 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,101 @@
+function LODTriangleGeometry(edgeSteps) {
+	THREE.Geometry.call(this);
+
+	//        /
+	//		 /  
+	//      / /\ v
+	//     / /  \ \
+	//    / /    \ \
+	//   u|/______\ \
+	//  --+-------w  \
+	//    |           \
+
+	var rotThird = Math.PI * 2 / 3;
+	var uAngle = Math.PI + rotThird * .5;
+	var vAngle = uAngle + rotThird;
+	var wAngle = vAngle + rotThird;
+	var length = .05;
+	edgeSteps = edgeSteps || 12;
+	var u = new THREE.Vector2(Math.cos(uAngle), Math.sin(uAngle));
+	var v = new THREE.Vector2(Math.cos(vAngle), Math.sin(vAngle));
+	var w = new THREE.Vector2(Math.cos(wAngle), Math.sin(wAngle));
+	u.multiplyScalar(length);
+	v.multiplyScalar(length);
+	w.multiplyScalar(length);
+	var uStep = u.clone();
+	var vStep = v.clone();
+	var wStep = w.clone();
+	var stepLength = 1 / edgeSteps;
+	uStep.multiplyScalar(stepLength);
+	vStep.multiplyScalar(stepLength);
+	wStep.multiplyScalar(stepLength);
+
+	var cursor = new THREE.Vector2();
+	cursor.sub(v.clone().multiplyScalar(1/3)).add(w.clone().multiplyScalar(1/3));
+	var sideWaysLeft = wStep;
+	var sideWaysRight = wStep.clone().multiplyScalar(-1);
+	var sideWays = sideWaysLeft;
+	var upLeft = vStep;
+	var upRight = uStep.clone().multiplyScalar(-1);
+	var up = upLeft;
+
+	this.vertices.push(new THREE.Vector3(cursor.x, 0, cursor.y));
+	for (var iRowLength = edgeSteps; iRowLength >= 0; iRowLength--) {
+		sideWays = sideWays === sideWaysRight ? sideWaysLeft : sideWaysRight;
+		up = up === upRight ? upLeft : upRight;
+		for (var i = iRowLength; i > 0; i--) {
+			cursor.add(sideWays);
+			this.vertices.push(new THREE.Vector3(cursor.x, 0, cursor.y));
+		};
+		// cursor.add(sideWays);
+		// this.vertices.push(new THREE.Vector3(cursor.x, 0, cursor.y));
+		cursor.add(up);
+		if(iRowLength > 0) {
+			this.vertices.push(new THREE.Vector3(cursor.x, 0, cursor.y));
+		}
+	}
+
+	var cursor = 0;
+	for (var iRowLength = edgeSteps; iRowLength >= 0; iRowLength--) {
+		cursor += iRowLength;
+		// console.log(iRowLength, cursor);
+		var row = [];
+		var lastRow;
+
+		if(edgeSteps == 3) {
+			console.log(iRowLength, this.vertices.length);
+		}
+		for (var iFace = iRowLength; iFace > 0; iFace--) {
+			var d = cursor + iFace - iRowLength * 2 - 2;
+			var c = cursor + iFace;
+			var b = cursor - iFace+1;
+			var a = cursor - iFace;
+			if(edgeSteps == 3) {
+				console.log(iRowLength, ':', a, b, c);
+				console.log(iRowLength, ':', a, b, d);
+			}
+			if(a >= 0 && a < this.vertices.length && 
+				b >= 0 && b < this.vertices.length
+			) {
+				if((edgeSteps-iRowLength) % 2 == 0) {
+					var temp = a;
+					a = b;
+					b = temp;
+				}
+				if(c >= 0 && c < this.vertices.length) {
+					this.faces.push(new THREE.Face3(a, b, c));
+				}
+				if(d >= 0 && d < this.vertices.length) {
+					this.faces.push(new THREE.Face3(b, a, d));
+				}
+				// console.log(a, b, c);
+			}
+		};
+		cursor++;
+	};
+}
+
+
+LODTriangleGeometry.prototype = Object.create(THREE.Geometry.prototype);
+
+module.exports = LODTriangleGeometry;
---
+++ test.js
@@ -0,0 +1,79 @@
+var loadAndRunScripts = require('loadandrunscripts');
+var ManagedView = require('threejs-managed-view');
+loadAndRunScripts(
+	[
+		'bower_components/three.js/three.js',
+		'lib/stats.min.js',
+		'lib/threex.rendererstats.js'
+	],
+	function() {
+		var LODTriangleGeometry = require('./');
+		var Pallette = require('imagecolorpallette');
+		var view = new ManagedView.View({
+			stats:true
+		});
+
+		view.camera.position.x = 0;
+		view.camera.position.y = 1;
+		view.camera.position.z = 0;
+		view.camera.lookAt(new THREE.Vector3());
+		view.camera.near = .0001;
+		//lights
+		var light = new THREE.PointLight(0xffffff, 3);
+		view.scene.add(light);
+		var hemisphereLight = new THREE.HemisphereLight(0x7f6f5f, 0x7f0000);
+		view.scene.add(hemisphereLight);
+
+
+		var spin = new THREE.Object3D();
+		view.scene.add(spin);
+		new Pallette('assets/pallette.png', function(pallettes) {
+
+			var colors = pallettes[~~(pallettes.length * Math.random())];
+			// view.renderer.setClearColor(new THREE.Color(colors.splice(0, 1)[0]), 1);
+			var cursor = new THREE.Vector3();
+			// var mat = new THREE.MeshPhongMaterial();
+			for (var i = 4; i >= 0; i--) {
+				var steps = i + 1;
+				console.log(steps, new THREE.Color(colors[i]));
+				var lodTriangleGeometry = new LODTriangleGeometry(steps);
+				var lodTriangle = new THREE.Mesh(
+					lodTriangleGeometry,
+					new THREE.MeshBasicMaterial({
+						wireframe: true,
+						opacity: .5,
+						// transparent: true,
+						color: colors[i]
+					})
+				);
+				lodTriangle.materialID = i;
+				lodTriangle.scale.set(10, 10, 10);
+				lodTriangle.position.copy(cursor);
+				spin.add(lodTriangle);
+
+				var lodTrianglePCGeometry = new LODTriangleGeometry(steps);
+				var lodTrianglePC = new THREE.PointCloud(
+					lodTrianglePCGeometry,
+					new THREE.PointCloudMaterial({
+						size: .004 * (colors.length-i),
+						opacity: .5,
+						// transparent: true,
+						color: colors[i]
+					})
+				);
+				lodTrianglePC.materialID = i;
+				lodTrianglePC.scale.set(10, 10, 10);
+				lodTrianglePC.position.copy(cursor);
+				spin.add(lodTrianglePC);
+
+				cursor.y -= .008;
+			}
+		});
+
+		function onEnterFrame() {
+
+			spin.rotation.y += .01;
+		}
+		view.renderManager.onEnterFrame.add(onEnterFrame);
+	}
+)
